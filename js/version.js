c43ad42a07d845016160fcec3b0b02cda60b669b
EliteMay <k12m45k@gmail.com>
2026-08-30 10:07:17 +0900

    Bump version for split answer review

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.1',build:'20260830-3',dataSchema:4,analysisSchema:2,submissionPackage:2});
+window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.2',build:'20260830-4',dataSchema:4,analysisSchema:2,submissionPackage:2});
