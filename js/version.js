8a924b0f8d2ed1234638671ec6ffff0d3f608ae4
EliteMay <k12m45k@gmail.com>
2026-08-30 10:44:44 +0900

    Bump version to v0.5.5

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.4',build:'20260830-6',dataSchema:4,analysisSchema:2,submissionPackage:2});
+window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.5',build:'20260830-7',dataSchema:4,analysisSchema:2,submissionPackage:2});
