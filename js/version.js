cb366c1941e241e178fde995db5757525c097d2b
EliteMay <k12m45k@gmail.com>
2026-08-30 10:23:22 +0900

    Bump version to v0.5.3

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.2',build:'20260830-4',dataSchema:4,analysisSchema:2,submissionPackage:2});
+window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.3',build:'20260830-5',dataSchema:4,analysisSchema:2,submissionPackage:2});
