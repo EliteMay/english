451ebec90373ff51aeab30f4b97b3f2456b82259
EliteMay <k12m45k@gmail.com>
2026-08-30 09:50:16 +0900

    Bump English Worksheet Lab to v0.5.1

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.0',build:'20260830-2',dataSchema:4,analysisSchema:2,submissionPackage:2});
+window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.1',build:'20260830-3',dataSchema:4,analysisSchema:2,submissionPackage:2});
