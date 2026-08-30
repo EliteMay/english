92de1096dd7cf7b14c7a846790674fc1c242ed37
EliteMay <k12m45k@gmail.com>
2026-08-30 18:12:50 +0900

    Add single runtime metadata source

---
+++ js/app/meta.js
@@ -0,0 +1,13 @@
+export const APP=Object.freeze({
+  version:'v0.6.2',
+  build:'20260830-10',
+  stateSchema:6,
+  paperSchema:6,
+  analysisSchema:2,
+  submissionPackage:3
+});
+
+export const PROJECT=Object.freeze({
+  guideVersion:'1.1.0',
+  profiles:Object.freeze(['STATIC','DATA','MEDIA','AI-HANDOFF','TOOL'])
+});
