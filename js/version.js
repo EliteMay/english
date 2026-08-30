6cb911e2557e4b86e3823064d561064f88cf3629
EliteMay <k12m45k@gmail.com>
2026-08-30 10:37:56 +0900

    Bump to v0.5.4 for review geometry fix

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.3',build:'20260830-5',dataSchema:4,analysisSchema:2,submissionPackage:2});
+window.EnglishWorksheetBuild=Object.freeze({version:'v0.5.4',build:'20260830-6',dataSchema:4,analysisSchema:2,submissionPackage:2});
